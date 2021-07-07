0ce473de5e29dc8bad875aa44bcf7f52be6f5304
Khaled <achechkhaled@gmail.com>
2021-07-07 11:28:21 +0200

    Fixing module name
    Posts => Proudcts

--- routes/products.js
+++ routes/products.js
@@ -5,11 +5,11 @@ const router = express.Router();
 //ROUTES
 router.get('/',(req, res)=>
 {
-    res.send('We are on posts');
+    res.send('We are on products');
 });
 
 router.get('/specific',(req, res)=>
 {
-    res.send('We are on specific posts');
+    res.send('We are on specific products');
 });
 module.exports = router;
